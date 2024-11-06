00e7065a14b43f8203fcfd564251443cc10d0aef
apoowa <159249955+apoowa@users.noreply.github.com>
2024-11-06 09:10:36 +0000

    final_hanackova

---
+++ main.py
@@ -0,0 +1,12 @@
+array = ["auto", "mesic", "jablko", "sloup", "barva"]
+#pridani 
+array.append("papir")
+#smazani
+array.remove ("barva")
+#zameneni
+array [4] = "slunce"
+#ukol 5
+array2 = ["stringhod1", "stringhod2"]
+array.extend (array2)
+#vypis
+print (array)
